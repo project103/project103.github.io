ce3ff96a7f556eed4d902d6f790ba327a8e1c7b5
project103 <105249726+project103@users.noreply.github.com>
2022-05-10 02:30:58 +0200

    Add files via upload

---
+++ jv/func login().js
@@ -0,0 +1,7 @@
+function myaccount()
+{
+document.getElementById("username2").innerHTML= 'abdallah';
+document.getElementById("password2").innerHTML='abdo';
+document.getElementById("email2").innerHTML='abcd@gmail.com';
+document.getElementById("phone2").innerHTML='011457188975';
+}
